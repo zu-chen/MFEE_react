41da18ecfab9eddd4f4b7db5f4c718a73341b2c9
Zu <a2468929100@gmail.com>
2021-01-22 00:07:22 +0800

    0121

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [total, setTotal] = useState(0) // 解構賦值
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1
+      onClick={() => {
+        setTotal(total + 1)
+      }}
+    >
+      {total}
+    </h1>
+  )
 }
 
-export default App;
+export default App
